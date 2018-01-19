2181dc5885a96bf0d2350caaf3a17b992a91f5c9
dario_manocchio <31663365+dario9855@users.noreply.github.com>
2018-01-19 14:11:25 -0500

    part 1 done
    lable changes to text field ext when button is clicked

---
+++ ui_tutorial/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ui_tutorial
+//
+//  Created by Manocchio, Dario on 2018-01-19.
+//  Copyright © 2018 Manocchio, Dario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Handle the text fields's user input through delegate callbacks
+        mealTextField.delegate = self
+    }
+    //UI Elements
+    @IBOutlet weak var mealTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text=textField.text
+    }
+    //Actions
+    @IBAction func changeMealNameBtn(_ sender: UIButton) {
+        textFieldDidEndEditing(mealTextField)
+        
+    }
+
+
+}
+
---
+++ ui_tutorialTests/ui_tutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  ui_tutorialTests.swift
+//  ui_tutorialTests
+//
+//  Created by Manocchio, Dario on 2018-01-19.
+//  Copyright © 2018 Manocchio, Dario. All rights reserved.
+//
+
+import XCTest
+@testable import ui_tutorial
+
+class ui_tutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
